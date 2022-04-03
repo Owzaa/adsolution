b0d891d33e68ace84675d1b9d572fa8ebac772d3
Owzaa <nyondo.olwethu@gmail.comm>
2022-04-03 22:52:15 +0200

    commit on master

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import './App.css';
 import Home from './Components/Home/Home'
 function App() {
   return (
-    <div className="App">
+    <div className="Container">
 
-    
-
+    <div className='Logo__Header__Container'>ADS Amanzunza
+    <span className='Debt__Text'>Debt Solutions (PTY) LTD</span>
+     </div>
 
 
   <header className="App-header">
