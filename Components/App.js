1a5b2f450dadd647a6eb842f2b76fec847555c26
Owzaa <nyondo.olwethu@gmail.comm>
2022-04-03 15:00:41 +0200

    commit on master

--- Components/App.js
+++ Components/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Home from './Home/Home'
 function App() {
   return (
     <div className="App">
+
+    
+
+
+
   <header className="App-header">
-       
+
+      <Home/> 
 
   </header>
     </div>
